326f966fe6419b0e25d434c848b86897253acfbf
mazen1habib <mazenhabeb1@gmail.com>
2024-07-25 12:20:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const productroutes = require("./routes/productRouter");
+const tableroutes = require("./routes/tablesRouter.js");
+const userroutes = require("./routes/userRouter.js");
+const adminroutes = require("./routes/adminRouter.js");
+const path = require("node:path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+app.listen(process.env.PORT, () => {
+  console.log("server Started");
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("DB is connected"))
+  .catch((err) => console.log(err));
+app.use(express.static(path.join(__dirname, "/")));
+app.use("/api", tableroutes);
+app.use("/api", userroutes);
+app.use("/api", productroutes);
+app.use("/api", adminroutes);
